5ca4b30370bb6115668837940c5873762802b5f7
Pablito00 <91752643+Pablito00@users.noreply.github.com>
2022-01-04 16:01:07 +0000

    Add files via upload

--- emb/startup.c
+++ emb/startup.c
@@ -5,13 +5,32 @@
  *      Author: Admin
  */
 
+void measure_sonar{
+    I2CMSA = 0x0000001C0;
+    I2CMDR = 0x000000091;
+    I2MCS = 0x000000007;
 
-/*void start_I2C(){
+    while(!I2CMCS.BUSBSY);
 
+    if(I2CMCS.ERROR == 1)
+        return 1;
 
-}*/
+    return 0;
+}
 
-void start_mcu(){
+void get_value_sonar{
+    I2CMSA = 0x0000001C2;
+    I2MCS = 0x000000007;
+
+    while(!I2CMCS.BUSBSY);
+
+    if(I2CMCS.ERROR == 1)
+        return 1;
+
+    return 0;
+}
+
+void start_I2C(){
 
     /*perguntar aos outros caralhos o q eles vao inicializar*/
 
@@ -23,4 +42,5 @@ void start_mcu(){
     GPIOODR = 0x40004024;
     GPIOPCTL = 0X40004000;
     I2CMCR = 0x40020010
+    I2CMTPR = 0x00000009;
 }
